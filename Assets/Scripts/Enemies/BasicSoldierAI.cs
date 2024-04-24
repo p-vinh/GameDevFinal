166ea85fd9e7f62b50de9ceb0be1febb98ac5bd6
abbypin <abby@techcleanup.com>
2024-04-23 19:44:59 -0700

    Uploaded Lowpoly Characters

--- Assets/Scripts/Enemies/BasicSoldierAI.cs
+++ Assets/Scripts/Enemies/BasicSoldierAI.cs
@@ -8,48 +8,39 @@ public class BasicSoldierAI : MonoBehaviour {
     // Unity Editor Variables
     private NavMeshAgent mesh;
     private Transform enemy;
+    public GameObject fist;
     private Transform player;
     private Animator animator;
-    private Animator playerAnimator;
+    private PlayerStats playerStats;
 
     // Variables
-    public float speed = 5.0f; //can be changed for each type of soldier
-    public float increasedSpeed = 10.0f; //can be changed for each type of soldier
-    public float sightRange = 10.0f; //can be changed for Juggernaut
-    public float attackRange = 2.0f; //can be changed for each type of soldier (or attacks if we want)
-    public int punchDamage = 5; // basic soldier attack
-    public int health = 100; //can be changed for each type of soldier
-    private  int maxHealth = health;
-    private int halfHealth = maxHealth / 2;
-    private bool canTakeDamage = true;
+    public float speed = 5.0f; //can be changed for each type of soldier (running speed)
+    public float shieldHealth = 50.0f;
+    public float sightRange = 8.0f;
+    public float attackRange = 1.0f;
+    public float health = 100.0f; //can be changed for each type of soldier
+    public float punchDamage = 5.0f;
+
+    public bool canProtectWithShield = true;
+
+    // The Basic Soldier Enemy protects with a shield once the player is in sight range.
+    // The shield will take damage for the enemy until it is destroyed. Once the shield is destroyed,
+    // the enemy will attack with punch. The enemy will attack the player if the player is in attack range.
+    // The enemy will move to the player if the player is in sight range.
+    // The enemy will stop moving if the player is in attack range. The enemy will stop moving if the player is dead.
 
     // Setup Variables
     void Start() {
-        soldierType = GameObject.Tag;
-        mesh = GetComponent<NavMeshAgent>();
+        mesh = GetComponent<NavMeshAgent>(); // Get the Navigator
         enemy = GetComponent<Transform>();
-        player = GameObject.Find("Player").transform;
-        animator = GetComponent<Animator>();
-        playerAnimator = player.GetComponent<Animator>();
-        mesh.speed = speed;
-        damage = punchDamage;
+        player = GameObject.Find("Player").transform; // Find the player object
+        // Create a fist object attached to the enemy's hand position
+        // TODO: figure out how to work with an enemy fist (COuld just be animation and hand colision)
+        fist = Instantiate(fist, enemy.position, enemy.rotation);
+        animator = GetComponent<Animator>(); // Get the animator component
+        //playerStats = player.GetComponent<PlayerStats>(); // Get the player stats
+        mesh.speed = speed; // Set running speed
     }//end Start()
-
-    // Update is called once per frame
-    void Update() {
-        // Always look at player
-        enemy.LookAt(player.position);
-
-        // Move to Player if in sight range
-        if (checkSightRange()) {
-            MoveToPlayer();
-        }//end if
-
-        // Attack Player if in attack range
-        if (checkAttackRange()) {
-            AttackPlayer(animator, damage);
-        }//end if
-    }//end Update()
 
     // Boolean Helper Methods //
     bool checkSightRange() {
@@ -66,36 +57,75 @@ public class BasicSoldierAI : MonoBehaviour {
         return false;
     }//end checkAttackRange()
 
+    // Update is called once per frame
+    void Update() {
+        // Check Ranges
+        checkSightRange();
+        checkAttackRange();
+
+        // TODO: Always look at player
+
+        // Move to Player if in sight range
+        if (checkSightRange() && !checkAttackRange()) {
+            if (canProtectWithShield) {
+                ProtectWithShield();
+            }//end if
+            MoveToPlayer();
+        } else if (checkSightRange() && checkAttackRange()) {
+            AttackPlayer();
+            if (shieldHealth <= 0) {
+                canProtectWithShield = false;
+                AttackPlayer();
+            }//end if
+    }//end Update()
+
     // Universal Soldier AI Methods //
     // Move to Player AI
     void MoveToPlayer() {
         if (Vector3.Distance(transform.position, player.position) <= sightRange) {
             mesh.SetDestination(player.position);
-            enemy.LookAt(player.position);
+            // TODO: Always look at player
         }//end if
     }//end moveToPlayer()
 
-    // Attack Player if in range
-    void AttackPlayer(Animator animator, int damage) {
-        //attack player
-        mesh.SetDestination(transform.position);
-        enemy.LookAt(player.position);
-
-        if (Vector3.Distance(transform.position, player.position) <= attackRange) {
-            animator.SetTrigger(GetComponent<Animation>());
-
-            player.GetComponent<Player>().TakeDamage(damage);
+    void ProtectWithShield() {
+        if (shieldHealth > 0) {
+            // Shield takes damage
+            shieldHealth -= playerStats.Get(Damage); // TODO: Find out how to connect to player
+        } else if (shieldHealth <= 0) {
+            // Shield is destroyed
+            canProtectWithShield = false;
+            Destroy(shield);
         }//end if
-        Debug.Log("Human Enemy Attacking Player");
-    }//end attackPlayer()
+    }//end ProtectWithShield()
+
+    void AttackPlayer() {
+        // Check if the player is close to the enemy
+        if (checkAttackRange()) {
+            // Stop moving
+            mesh.SetDestination(enemy.position); // Stop moving
+            mesh.isStopped = true; // Stop the enemy
+
+            animator.SetTrigger("PunchAttack"); // Punch the player
+            // Player take Damage if the fist collides with the player
+            if (fist.GetComponent<Collider>().bounds.Intersects(player.GetComponent<Collider>().bounds)) {
+                playerStats.TakeDamage(punchDamage);
+                Debug.Log("Attack Player");
+            }//end if
+        }//end if
+    }//end AttackPlayer()
 
     // Take Damage if sheild is down
-    void EnemyTakeDamage() {
-        if(canTakeDamage) {
-            health -= punchDamage;
-            if (health <= 0) {
-                Destroy(gameObject);
-            }//end if
+    void TakeDamage() {
+        if (shieldHealth <= 0) {
+            health -= playerDamage; // Player attacks the enemy
+            canProtectWithShield = false; // Destroy the shield
+            Destroy(shield); // Destroy the shield
+        }//end if
+
+        // Check if the enemy is dead
+        if (health <= 0) {
+            Destroy(gameObject); // Kill the enemy
         }//end if
     }//end EnemyTakeDamage()
 }//end BasicSoldierAI
