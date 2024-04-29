2246b2511a0c6d84fa3c885c3beab1a5bdf05ef1
abbypin <abby@techcleanup.com>
2024-04-28 22:45:31 -0700

    Uploaded General Human Prefab

--- Assets/Audio/AudioScripts/AudioLoopController.cs
+++ Assets/Audio/AudioScripts/AudioLoopController.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class AudioLoopController : MonoBehaviour {
     public AudioSource audioSource;
     public float playTime = 120.0f; // Set the play time to 2mins
+    public float startTime = 0.0f; // Set the start time to 0 seconds
 
     private void Start() {
         // Get the AudioSource component
         audioSource = GetComponent<AudioSource>();
+
+        audioSource.time = startTime;
 
         // Play the audio clip initially
         audioSource.Play();
@@ -25,11 +28,14 @@ public class AudioLoopController : MonoBehaviour {
         if (audioSource.time >= playTime) {
             // Stop the audio clip
             audioSource.Stop();
+
+            audioSource.time = startTime;
             audioSource.Play();
         }//end if
 
         if (!audioSource.isPlaying) {
             // Restart the audio clip
+            audioSource.time = startTime;
             audioSource.Play();
         }//end if
     }//end Update()
