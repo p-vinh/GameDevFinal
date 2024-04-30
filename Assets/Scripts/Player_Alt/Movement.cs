ed5c543415d3a5908ceadbcf1726ecf6f3f13069
LaurenceT123 <laurencetremblay10@gmail.com>
2024-04-29 23:49:13 -0700

    changed movement script to follow input manager

--- Assets/Scripts/Player_Alt/Movement.cs
+++ Assets/Scripts/Player_Alt/Movement.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PlayerController.InputsManager;
 
 public class Movement : MonoBehaviour
 {
-
+    
     Rigidbody rigidBody;
     public float speed = 4;
     Animator anim;
@@ -12,11 +13,20 @@ public class Movement : MonoBehaviour
     public bool carryGun;
     public bool carrySword;
     private bool attackAnimDone = true;
+
+
+    //Added input manager 
+    public Camera m_Camera = null;
+    public InputsManager m_InputsManager;
+    private Vector3 m_MoveDirection;
+    
     // Start is called before the first frame update
     void Start()
     {
+        m_Camera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
         rigidBody = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
+        m_InputsManager = GetComponent<InputsManager>();
     }
 
     void Update()
@@ -24,19 +34,18 @@ public class Movement : MonoBehaviour
         bool isShiftKeyDown = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
 
         //Point a ray from mouse to camera, use this to rotate player to look at mouse
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //print(m_InputsManager._MousePosition);
+        Ray ray = Camera.main.ScreenPointToRay(m_InputsManager._MousePosition);
         RaycastHit hit;
 
-        if(Physics.Raycast(ray, out hit, 100))
+        if (Physics.Raycast(ray, out hit,100))
         {
-            lookPos = hit.point;
+            Vector3 targetDirection = hit.point - transform.position;
+            targetDirection.y = 0;
+
+            transform.LookAt(transform.position + targetDirection, Vector3.up);
+            //transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * m_PlayerRotationSpeed);
         }
-
-        Vector3 lookDir = lookPos - transform.position;
-
-        lookDir.y = 0; //prevents player looking up
-
-        transform.LookAt(transform.position + lookDir, Vector3.up);
 
         //If sprint mode, change anim to run
         if(isShiftKeyDown)
@@ -66,16 +75,18 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal") * speed;
-        float vertical = Input.GetAxisRaw("Vertical") * speed;
+        //m_MoveDirection = CalculateDirection();
+        //m_Rigidbody.velocity = m_MoveDirection * m_PlayerSpeed;
+
+        m_InputsManager.HandleInputs();
 
         if(attackAnimDone)
         {
-            rigidBody.velocity = new Vector3(horizontal,0,vertical);
+            rigidBody.velocity = new Vector3(m_InputsManager._HorizontalInput*speed, 0, m_InputsManager._VerticalInput * speed);
         }
 
-        anim.SetFloat("Forward",vertical);
-        anim.SetFloat("Turn",horizontal);
+        anim.SetFloat("Forward",m_InputsManager._HorizontalInput);
+        anim.SetFloat("Turn",m_InputsManager._VerticalInput);
        
     }
 
@@ -98,5 +109,18 @@ public class Movement : MonoBehaviour
     {
         anim.SetBool("Attack",false);
         attackAnimDone = true;
+    }
+
+    private Vector3 CalculateDirection()
+    {
+        Vector3 vector = Vector3.zero;
+        //vector = m_Camera.transform.forward * m_InputsManager._VerticalInput; 
+        vector = Vector3.forward * m_InputsManager._VerticalInput;
+
+        //vector = vector + m_Camera.transform.right * m_InputsManager._HorizontalInput;
+        vector = vector + Vector3.right * m_InputsManager._HorizontalInput;
+        vector.Normalize();
+        vector.y = 0;
+        return vector;
     }
 }
