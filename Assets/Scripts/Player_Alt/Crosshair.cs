071eef476089a1d20a744bb0c9d05ce91099171c
LaurenceT123 <laurencetremblay10@gmail.com>
2024-04-29 23:55:26 -0700

    crosshair script also follows inputmanager now

--- Assets/Scripts/Player_Alt/Crosshair.cs
+++ Assets/Scripts/Player_Alt/Crosshair.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PlayerController.InputsManager;
 
 public class Crosshair : MonoBehaviour
 {
 
     Vector3 pos;
     public float speed = 1f;
+    public InputsManager m_InputsManager;
 
+    void Start()
+    {
+        m_InputsManager = GetComponent<InputsManager>();
+    }
     // Update is called once per frame
     void Update()
     {
-        pos = Input.mousePosition;
+        pos = m_InputsManager._MousePosition;
         pos.z = speed;
         transform.position = Camera.main.ScreenToWorldPoint(pos);
     }
